1023497615f190062f0871f0877b2bc8a97bb40b
Haru-arp <vr1997@naver.com>
2022-10-17 16:48:47 +0900

    init project

---
+++ nolmung/App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+const App = () => {
+  return(
+      <View>
+        <Text>Hello World</Text>
+   
+      </View>
+  )
+ 
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
