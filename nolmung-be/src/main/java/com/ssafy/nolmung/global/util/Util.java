8bccb91b563f1eac6cb1036803d75e1fb8f6258d
이지영 <jioung93@naver.com>
2022-10-31 01:02:12 +0900

    [S07P31A502-82] feat: 게시물 이미지 업로드 util 함수 구현

--- nolmung-be/src/main/java/com/ssafy/nolmung/global/util/Util.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/global/util/Util.java
@@ -1,5 +1,50 @@
 package com.ssafy.nolmung.global.util;
 
+import com.ssafy.nolmung.board.domain.BoardImage;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
 public class Util {
+
+    static class ImageUtil {
+
+        private String uploadPath;
+
+        public List<String> uploadBoardImages(int boardId, List<MultipartFile> files) {
+
+            List<String> result = new ArrayList<>();
+            for (MultipartFile file : files) {
+                if(file.isEmpty() || !file.getContentType().startsWith("image")){
+                    return null; // 에러 처리 필요
+                }
+
+                String fileName = UUID.randomUUID().toString();
+                String originFileName = file.getOriginalFilename();
+                fileName += originFileName.substring(originFileName.lastIndexOf("."));
+
+                File f = new File(fileName);
+                // 에러 처리 필요
+                try {
+                    file.transferTo(f);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                result.add(uploadPath+"/"+fileName);
+            }
+
+            return result;
+        }
+
+
+
+    }
 
 }
