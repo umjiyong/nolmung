556d4cf0a286eb9374da78a7af8d164a754d49a1
이지영 <jioung93@naver.com>
2022-10-31 16:42:33 +0900

    fix: 이미지 업로드 에러 수정

--- nolmung-be/src/main/java/com/ssafy/nolmung/global/util/Util.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/global/util/Util.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 
+@Component
 public class Util {
 
     @Component
--- nolmung-be/src/main/java/com/ssafy/nolmung/image/ImageRepository.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/image/ImageRepository.java
@@ -1,11 +1,14 @@
 package com.ssafy.nolmung.image;
 
+import com.ssafy.nolmung.board.domain.Board;
 import com.ssafy.nolmung.board.domain.BoardImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface ImageRepository extends JpaRepository {
+@Repository
+public interface ImageRepository extends JpaRepository<BoardImage, Integer> {
 
     BoardImage save(BoardImage boardImage);
     List<BoardImage> findAllByBoard_BoardId(int boardId);
