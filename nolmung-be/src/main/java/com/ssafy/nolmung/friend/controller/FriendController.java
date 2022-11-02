a941a06324f9eb777f52209b7ae5a91416cdae32
chan449 <chan449@naver.com>
2022-11-02 11:23:59 +0900

    Fix friend merge conflicts

--- nolmung-be/src/main/java/com/ssafy/nolmung/friend/controller/FriendController.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/friend/controller/FriendController.java
@@ -1,29 +1,34 @@
-//package com.ssafy.nolmung.friend.controller;
-//
-//import com.ssafy.nolmung.friend.service.BlockService;
-//import com.ssafy.nolmung.friend.service.FriendService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/friend")
-//@Slf4j
-//public class FriendController {
-//
-//    private final FriendService friendService;
-//    private final BlockService blockService;
-//
-//
-//    @DeleteMapping("/{friendId}")
-//    public void deleteFriend (@RequestParam("friendId") int friendId) {
-//
-//        friendService.delete(friendService.findById(friendId));
-//
-//    }
-//
-//}
+package com.ssafy.nolmung.friend.controller;
+
+import com.ssafy.nolmung.friend.service.BlockService;
+import com.ssafy.nolmung.friend.service.FriendService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/friend")
+@Slf4j
+public class FriendController {
+
+    private final FriendService friendService;
+    private final BlockService blockService;
+
+    @GetMapping("/{userId}")
+    public void getFriendList (@RequestParam("userId") int userId){
+
+
+
+
+    }
+
+
+    @DeleteMapping("/{friendId}")
+    public void deleteFriend (@RequestParam("friendId") int friendId) {
+
+        friendService.delete(friendService.findById(friendId));
+
+    }
+
+}
