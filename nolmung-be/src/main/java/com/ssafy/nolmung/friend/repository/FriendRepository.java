4fe7717687e6d02d7e2d4b5b8ba25216abff902f
chan449 <chan449@naver.com>
2022-11-02 11:26:16 +0900

    Fix friend merge conflicts

--- nolmung-be/src/main/java/com/ssafy/nolmung/friend/repository/FriendRepository.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/friend/repository/FriendRepository.java
@@ -1,35 +1,44 @@
-//package com.ssafy.nolmung.friend.repository;
-//
-//import com.ssafy.nolmung.friend.domain.Friend;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Repository;
-//
-//import javax.persistence.EntityManager;
-//
-//@Repository
-//@RequiredArgsConstructor
-//
-//public class FriendRepository {
-//
-//    private final EntityManager em;
-//
-//    public void regist (Friend friend) {
-//
-//        em.persist(friend);
-//
-//    }
-//
-//    public Friend findById (int id) {
-//
-//       return em.find(Friend.class,id);
-//
-//    }
-//
-//    public void delete (Friend friend) {
-//
-//        em.remove(friend);
-//
-//    }
-//
-//
-//}
+package com.ssafy.nolmung.friend.repository;
+
+import com.ssafy.nolmung.friend.domain.Friend;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+
+public class FriendRepository {
+
+    private final EntityManager em;
+
+    public void regist (Friend friend) {
+
+        em.persist(friend);
+
+    }
+
+    public Friend findById (int id) {
+
+       return em.find(Friend.class,id);
+
+    }
+
+    public void delete (Friend friend) {
+
+        em.remove(friend);
+
+    }
+
+
+    public List<Friend> findFriendListByUserId(int userId) {
+
+        List<Friend> friendList =  em.createQuery("SELECT f FROM Friend f WHERE f.userId = :user_id",Friend.class)
+                .setParameter("user_id",userId).getResultList();
+
+
+        return friendList;
+    }
+}
--- nolmung-be/src/main/java/com/ssafy/nolmung/friend/service/FriendService.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/friend/service/FriendService.java
@@ -1,37 +1,47 @@
-//package com.ssafy.nolmung.friend.service;
-//
-//import com.ssafy.nolmung.friend.domain.Friend;
-//import com.ssafy.nolmung.friend.repository.FriendRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//@RequiredArgsConstructor
-//@Transactional(readOnly = true)
-//public class FriendService {
-//
-//    private final FriendRepository friendRepository;
-//
-//    @Transactional
-//    public void regist (Friend friend){
-//
-//        friendRepository.regist(friend);
-//
-//    }
-//    public Friend findById (int id) {
-//
-//        return friendRepository.findById(id);
-//
-//    }
-//
-//
-//    @Transactional
-//    public void delete (Friend friend) {
-//
-//        friendRepository.delete(friend);
-//
-//    }
-//
-//
-//}
+package com.ssafy.nolmung.friend.service;
+
+import com.ssafy.nolmung.friend.domain.Friend;
+import com.ssafy.nolmung.friend.repository.FriendRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FriendService {
+
+    private final FriendRepository friendRepository;
+
+    @Transactional
+    public void regist (Friend friend){
+
+        friendRepository.regist(friend);
+
+    }
+    public Friend findById (int id) {
+
+        return friendRepository.findById(id);
+
+    }
+
+    public List<Friend> findFriendListByUserId (int userId) {
+
+        return friendRepository.findFriendListByUserId(userId);
+
+    }
+
+
+
+
+    @Transactional
+    public void delete (Friend friend) {
+
+        friendRepository.delete(friend);
+
+    }
+
+
+}
