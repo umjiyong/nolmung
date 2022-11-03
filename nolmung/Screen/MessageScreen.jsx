d0a7b1f1be1b841a9ae2bfc37812ef3d58a3cf17
DESKTOP-KVCQHCD\SSAFY <rlaxorud814@gmail.com>
2022-11-03 14:16:46 +0900

    feat: message-firebase

--- nolmung/Screen/MessageScreen.jsx
+++ nolmung/Screen/MessageScreen.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Image, Text, View,ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  Image,
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import MessageRoom from '../Components/MessageRoom';
 const MessageScreen = () => {
   const navigation = useNavigation();
+
   return (
     <>
-    {/* Header */}
+      {/* Header */}
       <View style={Styles.Header}>
         <TouchableOpacity onPress={navigation.goBack}>
           <Image
@@ -20,13 +28,32 @@ const MessageScreen = () => {
         </TouchableOpacity>
         <Text style={Styles.HeaderText}>메세지</Text>
       </View>
-    {/* Header End */}
-    <ScrollView contentContainerStyle={{padding: 20}} showsVerticalScrollIndicator={false} style={Styles.messagnerContainer}>
-        <MessageRoom img={require('../assets/icons/33.png')} userName="옆집 눈나" messageTime="오후 8:10"/>
-        <MessageRoom img={require('../assets/icons/32.png')} userName="옆집 아줌마" messageTime="오후 7:10"/>
-        <MessageRoom img={require('../assets/icons/image22.png')} userName="옆집 형" messageTime="오후 6:10"/>
-        <MessageRoom img={require('../assets/icons/man1Avatar.png')} userName="옆집 형님" messageTime="오후 5:10"/>
-    </ScrollView>
+      {/* Header End */}
+      <ScrollView
+        contentContainerStyle={{padding: 20}}
+        showsVerticalScrollIndicator={false}
+        style={Styles.messagnerContainer}>
+        <MessageRoom
+          img={require('../assets/icons/33.png')}
+          userName="옆집 눈나"
+          messageTime="오후 8:10"
+        />
+        <MessageRoom
+          img={require('../assets/icons/32.png')}
+          userName="옆집 아줌마"
+          messageTime="오후 7:10"
+        />
+        <MessageRoom
+          img={require('../assets/icons/image22.png')}
+          userName="옆집 형"
+          messageTime="오후 6:10"
+        />
+        <MessageRoom
+          img={require('../assets/icons/man1Avatar.png')}
+          userName="옆집 형님"
+          messageTime="오후 5:10"
+        />
+      </ScrollView>
     </>
   );
 };
@@ -41,17 +68,15 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
   },
   HeaderText: {
-    textAlign:'center',
+    textAlign: 'center',
     color: '#282828',
     fontSize: 18,
     fontFamily: 'NotoSansKR-Bold',
     marginLeft: 15,
   },
   messagnerContainer: {
-    marginTop:25,
-    overflow: "visible"
+    marginTop: 25,
+    overflow: 'visible',
   },
-  messageRoom:{
-    
-  }
+  messageRoom: {},
 });
