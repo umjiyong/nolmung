eb0995d12ec08cbeb5f259e6104ed50f4e205fb7
chan449 <chan449@naver.com>
2022-11-11 09:33:37 +0900

    fix merge conflict

--- nolmung-be/src/main/java/com/ssafy/nolmung/global/config/Webconfig.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/global/config/Webconfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Webconfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
