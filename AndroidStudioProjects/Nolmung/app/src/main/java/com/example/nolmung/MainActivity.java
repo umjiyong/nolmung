a1065a1e06930cf8f2e39b612fd841fc6e4d1ed2
엄지용 <dogdg23@naver.com>
2022-11-16 15:28:00 +0900

    Feature : add running dog animation

--- AndroidStudioProjects/Nolmung/app/src/main/java/com/example/nolmung/MainActivity.java
+++ AndroidStudioProjects/Nolmung/app/src/main/java/com/example/nolmung/MainActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.nolmung.databinding.ActivityMainBinding;
+
+import org.w3c.dom.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,20 +21,36 @@ import java.util.TimerTask;
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    private TextView walkButtonTextView;
     private TimerTask timerTask;
     private ActivityMainBinding binding;
     private Timer timer = new Timer();
+    private Animation blinkAnim;
+    private ImageView runningDog;
+    private ImageView runningDogStop;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        mTextView = findViewById(R.id.textView2);
+        mTextView = findViewById(R.id.statusText);
+        walkButtonTextView = findViewById(R.id.btnWalk);
+        blinkAnim = AnimationUtils.loadAnimation(this,R.anim.blink_animation);
+        mTextView.startAnimation(blinkAnim);
 
 //        EditText editTextTime = binding.editTextTime;
+
+
+        ImageView runningDogStop = (ImageView) findViewById(R.id.gif_image);
+        Glide.with(this).load(R.drawable.running_dog_stop).into(runningDogStop);
+
+
+//        runningDog.
 
 
     }
@@ -41,24 +63,31 @@ public class MainActivity extends Activity {
 
     public void clickHandler(View view)
     {
-        switch(view.getId())
+        switch(walkButtonTextView.getText().toString())
         {
-            case R.id.btnStart:
+            case "산책 시작":
                 startTimerTask();
+                mTextView.clearAnimation();
+                ImageView runningDog = (ImageView) findViewById(R.id.gif_image);
+                Glide.with(this).load(R.drawable.running_dog).into(runningDog);
+
+
                 break;
-            case R.id.btnReset :
-                stopTimerTask();
+            case "산책 중지":
+                endTimerTask();
+                ImageView runningDogStop = (ImageView) findViewById(R.id.gif_image);
+                Glide.with(this).load(R.drawable.running_dog_stop).into(runningDogStop);
                 break;
         }
     }
 
     private void startTimerTask()
     {
-        stopTimerTask();
+
 
         timerTask = new TimerTask()
         {
-            int count = 0;
+            int count = -1;
 
             @Override
             public void run()
@@ -67,7 +96,8 @@ public class MainActivity extends Activity {
                 mTextView.post(new Runnable() {
                     @Override
                     public void run() {
-                        mTextView.setText(count + " 초");
+                        mTextView.setText(count/60 +" 분  " + count%60  + " 초");
+                        walkButtonTextView.setText("산책 중지");
                     }
                 });
             }
@@ -75,11 +105,12 @@ public class MainActivity extends Activity {
         timer.schedule(timerTask,0 ,1000);
     }
 
-    private void stopTimerTask()
+    private void endTimerTask()
     {
         if(timerTask != null)
         {
-            mTextView.setText("0 초");
+//            mTextView.setText("0 초");
+            walkButtonTextView.setText("산책 시작");
             timerTask.cancel();
             timerTask = null;
         }
