66f0ee199b627db5ef0c6622051b558de9fff9e6
이지영 <jioung93@naver.com>
2022-11-17 13:40:58 +0900

    feat: 행정동 이름으로 지역id를 반환해주는 repository 함수 추가

--- nolmung-be/src/main/java/com/ssafy/nolmung/region/repository/RegionRepository.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/region/repository/RegionRepository.java
@@ -1,4 +1,10 @@
 package com.ssafy.nolmung.region.repository;
 
-public interface RegionRepository {
+import com.ssafy.nolmung.region.domain.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+    Region findByEupMyeonDong(String eupMyeonDong);
 }
