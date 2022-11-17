3c55a860ece678f346526a9ce834f345a46b6d81
이지영 <jioung93@naver.com>
2022-11-17 13:07:44 +0900

    [S07P31A502-253] design: 원형 디자인 적용

--- nolmung_watch/app/src/main/java/com/example/nolmung_watch/PuppyAdapter.java
+++ nolmung_watch/app/src/main/java/com/example/nolmung_watch/PuppyAdapter.java
@@ -3,16 +3,18 @@ package com.example.nolmung_watch;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.wear.widget.WearableRecyclerView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class PuppyAdapter extends RecyclerView.Adapter<PuppyAdapter.ViewHolder> {
+public class PuppyAdapter extends WearableRecyclerView.Adapter<PuppyAdapter.ViewHolder> {
 
     ArrayList<PuppyInfo> items = new ArrayList<>();
 
@@ -53,21 +55,19 @@ public class PuppyAdapter extends RecyclerView.Adapter<PuppyAdapter.ViewHolder> 
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView puppyImg;
         TextView puppyName;
-        TextView breedAge;
         ProgressBar puppyWalkNeeds;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             puppyName = itemView.findViewById(R.id.puppyName);
-            breedAge = itemView.findViewById(R.id.breedAge);
             puppyWalkNeeds = itemView.findViewById(R.id.puppyWalkNeeds);
         }
 
         public void setItem(PuppyInfo item) {
             puppyName.setText(item.getPuppyName());
-            breedAge.setText(item.getBreedAge());
             puppyWalkNeeds.setProgress(Integer.parseInt(item.getPuppyWalkNeeds()));
         }
     }
