5d35b489775aa657e8a8d5d7c96e59d5136f8b55
이지영 <jioung93@naver.com>
2022-11-18 11:05:11 +0900

    fix: firebase messaging 파트 삭제

--- nolmung/App.js
+++ nolmung/App.js
@@ -3,19 +3,10 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import StackNavigator from './src/navigation/StackNavigator';
-import messaging from '@react-native-firebase/messaging';
 
 import 'react-native-gesture-handler';
 
 const App = () => {
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
-
-    return unsubscribe;
-  }, []);
-
   return (
     <NavigationContainer>
       <StackNavigator />
