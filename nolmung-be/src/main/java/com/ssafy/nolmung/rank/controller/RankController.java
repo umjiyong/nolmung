643f62cbee8c223b4210adeea8e447374fcf60f1
엄지용 <dogdg23@naver.com>
2022-11-20 00:33:54 +0900

    chore : Rank Sys - added auto reset

--- nolmung-be/src/main/java/com/ssafy/nolmung/rank/controller/RankController.java
+++ nolmung-be/src/main/java/com/ssafy/nolmung/rank/controller/RankController.java
@@ -104,7 +104,7 @@ public class RankController {
 //        rankService.resetRankScore(rankCategory);
 //    }
 
-    @Scheduled (cron = "0 30 0 * * *")
+    @Scheduled (cron = "0 40 0 * * *")
     @DeleteMapping("/reset/daily")
     public void resetDailyRankScore () {
 
