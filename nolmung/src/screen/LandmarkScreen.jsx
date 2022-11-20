197cd5d125a1e07dda5ad4464ba5f8fa83c8b968
Haru-arp <vr1997@naver.com>
2022-11-21 04:48:04 +0900

    rebase

--- nolmung/src/screen/LandmarkScreen.jsx
+++ nolmung/src/screen/LandmarkScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Image,
@@ -13,50 +13,53 @@ import GoBackHeader from '../components/GoBackHeader';
 import VisitMemo from '../components/VisitMemo';
 import Visitors from '../components/Visitors';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {getUserInfo} from "../api/User"
-import {getLandmark,postLandmark,getLandmarkArticle,getLandmarkVisitor,getLandmarkArticleList} from "../api/Landmark"
-import {useNavigation} from '@react-navigation/native';
+import {getUserInfo} from '../api/User';
+import {
+  getLandmark,
+  postLandmark,
+  getLandmarkArticle,
+  getLandmarkVisitor,
+  getLandmarkArticleList,
+} from '../api/Landmark';
+import {useNavigation, useIsFocused} from '@react-navigation/native';
+
 function LandmarkScreen({navigation: {navigate}, route}) {
   const Navigation = useNavigation();
-  const [landmarkArticle,setlandmarkArticle] =useState([])
-  const [userInfo,setuserInfo] = useState([])
-  const [landmarkInfo,setlandmarkInfo] =useState([])
-  const [landmarkVisitor,setlandmarkVisitor] =useState([])
-  const [landmarkAritlelist,setlandmarkAritlelist] =useState([])
+  const isFocused = useIsFocused(); // isFoucesd Define
+
+  const [landmarkArticle, setlandmarkArticle] = useState([]);
+  const [userInfo, setuserInfo] = useState([]);
+  const [landmarkInfo, setlandmarkInfo] = useState([]);
+  const [landmarkVisitor, setlandmarkVisitor] = useState([]);
+  const [landmarkAritlelist, setlandmarkAritlelist] = useState([]);
+  const [test, setTest] = useState(0);
 
   const user_info_func = async () => {
     try {
       await AsyncStorage.getItem('userId', (err, id) => {
-        getUserInfo(
-          {id : 1},
-          response => {
-            setuserInfo(response.data);
-          },
-          
-        );
+        getUserInfo({id: id}, response => {
+          setuserInfo(response.data);
+        });
       });
     } catch (err) {
       console.log('유저정보페이지 유저정보 get 에러', err);
     }
   };
-
 
   const getLandmarkArticleFunc = async () => {
     try {
       await AsyncStorage.getItem('userId', (err, id) => {
         getLandmarkArticle(
-          {landmarkBoardId : route.params.landmarkId},
+          {landmarkBoardId: route.params.landmarkId},
           response => {
             setlandmarkArticle(response.data);
           },
-          
         );
       });
     } catch (err) {
       console.log('유저정보페이지 유저정보 get 에러', err);
     }
   };
-
 
   const getLandmarkFunc = async () => {
     try {
@@ -64,12 +67,11 @@ function LandmarkScreen({navigation: {navigate}, route}) {
         getLandmark(
           {
             landmarkId: route.params.landmarkId,
-            userId: 1 // userid 1 로 되있다면 어싱크로 바꿔야
+            userId: id, // userid 1 로 되있다면 어싱크로 바꿔야
           },
           response => {
             setlandmarkInfo(response.data);
           },
-          
         );
       });
     } catch (err) {
@@ -77,18 +79,14 @@ function LandmarkScreen({navigation: {navigate}, route}) {
     }
   };
 
-  
-
-
   const getLandmarkArticleListFunc = async () => {
     try {
       await AsyncStorage.getItem('userId', (err, id) => {
         getLandmarkArticleList(
-          {landmarkId : route.params.landmarkId},
+          {landmarkId: route.params.landmarkId},
           response => {
             setlandmarkAritlelist(response.data);
           },
-          
         );
       });
     } catch (err) {
@@ -96,19 +94,12 @@ function LandmarkScreen({navigation: {navigate}, route}) {
     }
   };
 
-
-
-
   const getLandmarkVisitorFunc = async () => {
     try {
       await AsyncStorage.getItem('userId', (err, id) => {
-        getLandmarkVisitor(
-          {landmarkId : route.params.landmarkId},
-          response => {
-            setlandmarkVisitor(response.data);
-          },
-          
-        );
+        getLandmarkVisitor({landmarkId: route.params.landmarkId}, response => {
+          setlandmarkVisitor(response.data);
+        });
       });
     } catch (err) {
       console.log('랜드마크 방문자 get 에러', err);
@@ -121,95 +112,124 @@ function LandmarkScreen({navigation: {navigate}, route}) {
     getLandmarkArticleListFunc();
   }, []);
 
+  useEffect(() => {
+    getLandmarkArticleListFunc();
+  }, [isFocused]);
+
   // useEffect(() => {
-    
+
   //   getLandmarkArticleListFunc();
   // }, [landmarkAritlelist]);
-  
-  console.log("랜드마크 아티클리스트",landmarkAritlelist.landmarkBoardList)
+
+  console.log('랜드마크 아티클리스트', landmarkAritlelist.landmarkBoardList);
   return (
     <>
-    
-      {landmarkInfo.landmarkInfo ? <>
-      <GoBackHeader />
+      {landmarkInfo.landmarkInfo ? (
+        <>
+          <GoBackHeader />
 
-      <View style={{flex: 1}}>
-        {/* Header start */}
-        <View style={style.Header}>
-          <Text style={{color: '#282828', fontWeight: '600', fontSize: 18}}>
-            {landmarkInfo.landmarkInfo.landmarkName} 
-          </Text>
-          {/* <Image
+          <View style={{flex: 1}}>
+            {/* Header start */}
+            <View style={style.Header}>
+              <Text style={{color: '#282828', fontWeight: '600', fontSize: 18}}>
+                {landmarkInfo.landmarkInfo.landmarkName}
+              </Text>
+              {/* <Image
             source={require('../assets/icons/Vector.png')}
             resizeMode="contain"
           /> */}
-        </View>
-        {/* Header End */}
-        {/* 이미지 */}
-        {landmarkInfo.landmarkInfo.landmarkImg ? <Image 
-                    source={{uri : landmarkInfo.landmarkInfo.landmarkImg}}
-                    resizeMode="cover"
-                    style={{
-                      width: '100%',
-                    }}
-                /> : <Image 
-                    source={require('../assets/icons/background.png')}
-                    resizeMode="cover"
-                    style={{
-                      width: '100%',
-                    }}
-                /> }
-      
-        <View
-          style={{justifyContent: 'center', paddingLeft: 20, marginTop: 10}}>
-          <Text
-            style={{
-              color: '#282828',
-              fontSize: 18,
-              fontWeight: '600',
-              marginBottom: 10,
-            }}>
-            방문한 사람
-          </Text>
-          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-      
-            {landmarkVisitor.visitorList ? (
-              <>
-                {landmarkVisitor.visitorList.map((item, index) => {
-                  return <Visitors key={index} profileImg = {item.profileImg} />;
-                })}
-              </>
-            ) : <Text>사진이 등록되지 않았습니다</Text>}
-          </ScrollView>
-        </View>
-        <View style={style.memo}>
-          <Text style={{color: '#282828', fontSize: 18, fontWeight: '600'}}>
-            방명록
-          </Text>
-          <Pressable onPress={()=>{
-            Navigation.push('NewGuestBook',{landmarkId : landmarkInfo.landmarkInfo.landmarkId})
-          }} style={style.writebtn}>
-            <Text style={{color: '#fff', fontWeight: '500'}}>글쓰기</Text>
-          </Pressable>
-        </View>
-        <ScrollView contentContainerStyle={{padding: 20}}>
+            </View>
+            {/* Header End */}
+            {/* 이미지 */}
+            {landmarkInfo.landmarkInfo.landmarkImg ? (
+              <Image
+                source={{uri: landmarkInfo.landmarkInfo.landmarkImg}}
+                resizeMode="cover"
+                style={{
+                  width: '100%',
+                }}
+              />
+            ) : (
+              <Image
+                source={require('../assets/icons/background.png')}
+                resizeMode="cover"
+                style={{
+                  width: '100%',
+                }}
+              />
+            )}
 
-        {landmarkAritlelist.landmarkBoardList ? (
-              <>
-                {landmarkAritlelist.landmarkBoardList.map((item, index) => {
-                  return(<VisitMemo key={index} 
-                  boardImage = {item.boardImage}
-                  content = {item.content}
-                  createDate = {item.createDate}
-                  landmarkBoardId = {item.landmarkBoardId}
-                  nickname = {item.nickname} />) 
-                })}
-              </>
-            ) : <Text>글이 등록되지 않았습니다</Text>}
-          
-        </ScrollView>
-      </View>
-    </> : null}
+            <View
+              style={{
+                justifyContent: 'center',
+                paddingLeft: 20,
+                marginTop: 10,
+              }}>
+              <Text
+                style={{
+                  color: '#282828',
+                  fontSize: 18,
+                  fontWeight: '600',
+                  marginBottom: 10,
+                }}>
+                방문한 사람
+              </Text>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}>
+                {landmarkVisitor.visitorList ? (
+                  <>
+                    {landmarkVisitor.visitorList.map((item, index) => {
+                      return (
+                        <Visitors key={index} profileImg={item.profileImg} />
+                      );
+                    })}
+                  </>
+                ) : (
+                  <Text>사진이 등록되지 않았습니다</Text>
+                )}
+              </ScrollView>
+            </View>
+            <View style={style.memo}>
+              <Text style={{color: '#282828', fontSize: 18, fontWeight: '600'}}>
+                방명록
+              </Text>
+              <Pressable
+                onPress={() => {
+                  Navigation.push('NewGuestBook', {
+                    landmarkId: landmarkInfo.landmarkInfo.landmarkId,
+                  });
+                }}
+                style={style.writebtn}>
+                <Text style={{color: '#fff', fontWeight: '500'}}>글쓰기</Text>
+              </Pressable>
+            </View>
+            <ScrollView contentContainerStyle={{padding: 20}}>
+              {landmarkAritlelist.landmarkBoardList ? (
+                <>
+                  {landmarkAritlelist.landmarkBoardList.map((item, index) => {
+                    {
+                      console.log('item', item);
+                    }
+                    return (
+                      <VisitMemo
+                        key={index}
+                        boardImage={item.boardImage}
+                        content={item.content}
+                        createDate={item.createDate}
+                        landmarkBoardId={item.landmarkBoardId}
+                        nickname={item.nickname}
+                      />
+                    );
+                  })}
+                </>
+              ) : (
+                <Text>글이 등록되지 않았습니다</Text>
+              )}
+            </ScrollView>
+          </View>
+        </>
+      ) : null}
     </>
   );
 }
